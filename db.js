d98dafa5b93120fc876611a04fea9902258c7a5d
vmt07816 <58446101+vmt07816@users.noreply.github.com>
2020-03-20 10:46:37 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://yourMongoDBURIGoesHere');
+mongoose.connect('mongodb+srv://vmt07186:F490tmddmk|@cluster0-dx384.mongodb.net/test?retryWrites=true&w=majority');
