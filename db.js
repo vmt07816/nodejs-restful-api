d89bd46ebd2305940896c67debfb08700d8ea16f
vmt07816 <58446101+vmt07816@users.noreply.github.com>
2020-03-20 12:23:21 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,2 +1,12 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://vmt07186:F490tmddmk|@cluster0-dx384.mongodb.net/test?retryWrites=true&w=majority');
+//var mongoose = require('mongoose');
+//mongoose.connect('mongodb+srv://vmt07186:F490tmddmk|@cluster0-dx384.mongodb.net/test?retryWrites=true&w=majority');
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://vmt07186:F490tmddmk|@cluster0-dx384.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
